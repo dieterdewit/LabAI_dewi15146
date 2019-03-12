e865895c82592d48a262c1f0e553c4eb9e3ead5a
Dieter de WIt <dewi15146@uvg.edu.gt>
2019-03-12 09:39:46 -0600

    Final

---
+++ LAB_AI/Assets/Agente.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Agente : MonoBehaviour
+{
+    public float HayTePillo;
+    public float HuevaTime;
+
+    private NavMeshAgent agent;
+    private float timer;
+
+    void OnEnable()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        timer = HuevaTime;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= HuevaTime)
+        {
+            Vector3 newPos = RandomN(transform.position, HayTePillo, -1);
+            agent.SetDestination(newPos);
+            timer = 0;
+        }
+    }
+
+    public static Vector3 RandomN(Vector3 origen, float distancia, int mask)
+    {
+        Vector3 direccion = Random.insideUnitSphere * distancia;
+
+        direccion += origen;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(direccion, out navHit, distancia, mask);
+
+        return navHit.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "putote")
+        {
+            Destroy(collision.gameObject);
+            collision.gameObject.GetComponent<Movimiento>().Upsi();
+        }
+
+    }
+}
---
+++ LAB_AI/Assets/Movimiento.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movimiento : MonoBehaviour
+{
+    public float speed;
+    public float HayTeCojo;
+
+    private NavMeshAgent bola;
+
+    Vector3 newPosition;
+
+    void Start()
+    {
+        bola = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit azote;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out azote))
+            {
+                bola.SetDestination(azote.point);
+            }
+        }
+    }
+
+    public void Upsi()
+    {
+
+    }
+}
